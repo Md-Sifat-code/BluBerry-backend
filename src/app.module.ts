1dd4e4cc6a4e35f0b188786e1bff6d07c08621e7
habibb2r <habibhk127@gmail.com>
2025-07-25 01:42:35 +0600

    Reformed

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,12 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-     isGlobal: true   
-    }), UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
