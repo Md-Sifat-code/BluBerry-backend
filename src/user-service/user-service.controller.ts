a854f822c98b5f91976392dc628543e2c04f666d
habibb2r <habibhk127@gmail.com>
2025-07-25 05:10:38 +0600

    Service Get

--- src/user-service/user-service.controller.ts
+++ src/user-service/user-service.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UserServiceService } from './user-service.service';
 import { UserServiceDto } from './user-service-dto/user-service-dto';
 
 @Controller('user-service')
 export class UserServiceController {
   constructor(private readonly userServiceService: UserServiceService) {}
+
+  @Get()
+  async getAllServices() {
+    return this.userServiceService.getAllServices();
+  }
+
+  @Get(':id')
+  async getServiceById(@Param('id') id: number) {
+    return this.userServiceService.getServiceById(+id);
+  }
 
   @Post('create')
   async createService(@Body() serviceData: UserServiceDto) {
--- src/user-service/user-service.service.ts
+++ src/user-service/user-service.service.ts
@@ -6,6 +6,18 @@ import { UserServiceDto } from './user-service-dto/user-service-dto';
 @Injectable()
 export class UserServiceService {
   constructor(private prisma: PrismaService) {}
+  async getAllServices(): Promise<UserServiceDto[]> {
+    const services = await (this.prisma as PrismaClient).userService.findMany();
+    return services as UserServiceDto[];
+  }
+
+  async getServiceById(id: number): Promise<UserServiceDto | null> {
+    const service = await (this.prisma as PrismaClient).userService.findUnique({
+      where: { id },
+    });
+    return service as UserServiceDto | null;
+  }
+
   async createService(
     serviceData: Prisma.UserServiceCreateInput,
   ): Promise<UserServiceDto> {
