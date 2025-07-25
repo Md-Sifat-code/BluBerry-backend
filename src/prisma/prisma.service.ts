f18d4eeac12e5d9436e8c3ee0efbb6203855b951
linux44gh6 <abireshan32@gmail.com>
2025-07-25 22:40:17 +0600

    add data

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,11 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from 'generated/prisma';
 
 @Injectable()
-<<<<<<< HEAD
 export class PrismaService extends PrismaClient {
-=======
-export class PrismaService extends PrismaClient implements OnModuleInit {
->>>>>>> 6943b6fd93c28b3a2b58a5c4c46a99253ce85a92
   constructor(private config: ConfigService) {
     super({
       datasources: {
@@ -17,11 +13,5 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
       },
     });
   }
-<<<<<<< HEAD
-=======
 
-  async onModuleInit() {
-    await this.$connect();
-  }
->>>>>>> 6943b6fd93c28b3a2b58a5c4c46a99253ce85a92
 }
